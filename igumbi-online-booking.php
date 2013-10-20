17b99859706dbcec2ae4bbfe9b025aa0967ff289
smtm <smtm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-20 07:53:22 +0000

    changed screenshot 2
    git-svn-id: https://plugins.svn.wordpress.org/igumbi-online-booking/trunk@790783 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- igumbi-online-booking.php
+++ igumbi-online-booking.php
@@ -15,6 +15,7 @@ function igumbi_booking_install()  {
   add_option("igumbi_hotel_id", '6BEA7AW', '', 'yes'); // Default is the igumbi Demo Hotel
   add_option("igumbi_language", 'de', '', 'yes');
   add_option("igumbi_wide", '0', '', 'yes');     
+  add_option("igumbi_custom_css", '', '', 'yes');     
 }
 
 
@@ -22,6 +23,8 @@ function igumbi_booking_remove() {
    delete_option('igumbi_language');
    delete_option('igumbi_hotel_id');
    delete_option('igumbi_wide');
+   delete_option('igumbi_custom_css');
+
 }
 
 add_shortcode("igumbi_dialog", "igumbi_dialog");
@@ -31,15 +34,28 @@ function igumbi_dialog() {
   return "<div id='free_rooms'></div>";
 }
 
-function igumbi_avform() { 
+function igumbi_avform($atts) {
+  extract(shortcode_atts(array(
+     'lang' =>get_option('igumbi_language'),
+     'wide' =>get_option('igumbi_wide'),
+     'test' =>false
+  ), $atts))
+  ;
   $str  = "";
-  $str .= "<div id='avform' style='height:240px;width:200px;'></div>";
+  $str .= "<div id='avform'></div>";
   $str .= "<script src='https://www.igumbi.net/seller/";
   $str .= get_option('igumbi_hotel_id');
-  $str .= "/de/start.js";
-  if(get_option('igumbi_wide')==1)
+  $str .= "/". $lang . "/start.js";
+  if( $wide ==1 or $wide =='true') {
     $str .= "?layout=wide";
+  } else {
+    $str .= "?layout=tall";
+  }
+  if ($test =='true') {
+    $str .= "&test=true";
+  }
   $str .= "'></script>";
+  $str .='<style>'. get_option('igumbi_custom_css').'</style>';
   return $str;
 }
       
@@ -52,66 +68,7 @@ if (is_admin()) {
 }
 
 function igumbi_admin_page() {
-?>
-  <div class="wrap">
-  <div id="icon-options-general" class="icon32"><br /></div>
-    <h2>igumbi Online Booking Tool Embedding Page</h2>
-    <form method="post" action="options.php">
-    <?php wp_nonce_field('update-options'); ?>
-      <table>
-        <tr valign="top">
-          <td style="width: 160px; padding: 5px 5px 7px 5px;">igumbi Hotel Code:</td>
-          <td style="padding: 5px 5px 7px 5px;">
-            <input name="igumbi_hotel_id" type="text" id="igumbi_hotel_id" size=6 maxlength=100 
-            value="<?php echo get_option('igumbi_hotel_id'); ?>"/>
-          <p class="description">The 6 character <b>hotel code</b> you will find on the igumbi.com <a href="https://www.igumbi.net/settings/mine?locale=en">settings page</a>.<br/> 
-            To see it you need an account and need to be logged in over at <a href="https://www.igumbi.net/login">igumbi</a>.<br/>
-            You can create a new <a href="http://www.igumbi.com/trial?locale=en">trial account</a>.</p>
-          </td>
-          
-        </tr>
-        <tr valign="top">
-          <td style="width: 160px; padding: 5px 5px 7px 5px;">igumbi Language Code:</td>
-          <td style="padding: 5px 5px 7px 5px;">
-            <input name="igumbi_language" type="text" id="igumbi_language" size=6 maxlength=100 
-            value="<?php echo get_option('igumbi_language'); ?>"/>
-            <p class="description">ISO language code: Supported languages are DE, EN, GR, RU, ES</p>            
-          </td>
-        </tr>
-        <tr valign="top">
-          <td style="width: 160px; padding: 5px 5px 7px 5px;">igumbi wide mode:</td>
-          <td style="padding: 5px 5px 7px 5px;">
-            <select id="igumbi_wide" name="igumbi_wide">
-              <option value="0" <?php if(get_option('igumbi_wide') == 0) echo "selected='selected'";?>>tall</option>
-              <option value="1" <?php if(get_option('igumbi_wide') == 1) echo "selected='selected'";?>>wide</option>
-           </select>
-           <p class="description">
-             Tall or wide mode: The tall mode should be used in the sidebar as a widget. <br/>
-             The wide mode you integrate into the header or the top of the body via the shortcode [igumbi_avform].
-           </p>            
-          </td>
-        </tr>
-
-        <tr valign="top">
-          <td colspan="2" style="padding: 5px 5px 7px 5px;">
-            <input type="hidden" name="action" value="update" />
-            <input type="hidden" name="page_options" value="igumbi_hotel_id,igumbi_language,igumbi_wide" />
-            <input type="submit" value="<?php _e('Save Changes') ?>" class="button button-primary" />
-          </td>
-        </tr>
-      </table>
-    </form>
-    
-    <h2>Getting started:</h2>
-    <ol>
-      <li>Sign-up for a <a href="http://www.igumbi.com/trial?locale=en">igumbi trial account</a> and enter the settings above.</li>
-      <li>Set up your property in igumbi: at least describe your productcategories and load a picture for each productcategory. Ensure your bookings have been entered so that the correct availability can be calculated before setting the booking tool live on the site.</li>
-      <li>Create a testpage (or update your site template) and enter the the shortcode: [igumbi_avform] and [igumbi_dialog].</li>
-      <li>Add the [igumbi_avform] to the sidebar widgets. Menu Appearance >> Widgets and pull it to the top under/above search - move it up high, as it will be your primary conversion goal.</li>
-      <li>Add the shortcode to your theme as the first item after <code>&lt;div id="content" role="main"&gt;</code> with <code>&lt;?php echo do_shortcode("[igumbi_dialog]");?&gt;</code></li>
-    </ol>    
-  </div>
-<?php
+  include 'includes/views/admin-page.php';
 }
 
 class igumbi_Widget extends WP_Widget {
